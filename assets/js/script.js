2edec196f6f843ea856f644bc525f6d2c46d58f0
Kellly <kcdonlan@yahoo.com>
2022-01-13 19:14:22 -0500

    input

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var locations = [];
+
+var locationFormEl = document.querySelector("#location");
+var locationInputEl = document.querySelector("#location");
+var locationSearchInputEl = document.querySelector("#location-searched");
+var currentWeatherEl = document.querySelector("#current-weather-container");
+var forcastEl = document.querySelector("#forecast");
+var fiveDayEl = document.querySelector("#five-day-forecast");
+var searchHistoryButtonEl = document.querySelector("#search-history-btn");
+
+// get value from input element
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var location = locationInputEl.value.trim();
+    if(location) {
+        getLocationWeather(location);
+        getFiveDay(location);
+        currentWeatherEl.textContent = "";
+        locationInputEl.value = "";
+    }
+    else {
+        alert("Please enter a location");
+    }
+    saveSearch();
+    searchHistory(location);
+}
